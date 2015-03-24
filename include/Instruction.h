8b82ae30812748ac1e5320f2679bd9d3aa8711ea
Forest Belton <forest@homolo.gy>
2015-03-23 18:32:48 -0700

    Fix instruction format

--- include/Instruction.h
+++ include/Instruction.h
@@ -6,22 +6,24 @@ union Instruction {
 
     struct {
         union {
-            unsigned funct : 6;
-            unsigned shamt : 5;
-            unsigned rd : 5;
-            unsigned rt : 5;
-            unsigned rs : 5;
-        } reg;
+            struct {
+                unsigned funct : 6;
+                unsigned shamt : 5;
+                unsigned rd : 5;
+                unsigned rt : 5;
+                unsigned rs : 5;
+            } reg;
 
-        union {
-            unsigned imm : 16;
-            unsigned rt : 5;
-            unsigned rs : 5;
-        } imm;
+            struct {
+                unsigned imm : 16;
+                unsigned rt : 5;
+                unsigned rs : 5;
+            } imm;
 
-        union {
-            unsigned addr : 26;
-        } jump;
+            struct {
+                unsigned addr : 26;
+            } jump;
+        };
 
         unsigned op : 6;
     };
